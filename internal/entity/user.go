a2c9b9c50d1b065df53b92bc60ec4d1d9c135477
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-11-15 21:17:57 -0300

    feat: add user create validations

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,6 +1,8 @@
 package entity
 
 import (
+	"errors"
+
 	"github.com/melkzsiqueira/water-gas-measurement/pkg/entity"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -12,6 +14,14 @@ type User struct {
 	Password string    `json:"-"`
 }
 
+var (
+	ErrNameIsRequired     = errors.New("name is required")
+	ErrEmailIsRequired    = errors.New("email is required")
+	ErrInvalidEmail       = errors.New("invalid email")
+	ErrPasswordIsRequired = errors.New("password is required")
+	ErrInvalidPassword    = errors.New("invalid password")
+)
+
 func NewUser(name, email, password string) (*User, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
@@ -19,16 +29,40 @@ func NewUser(name, email, password string) (*User, error) {
 		return nil, err
 	}
 
-	return &User{
+	user := &User{
 		ID:       entity.NewID(),
 		Name:     name,
 		Email:    email,
 		Password: string(hash),
-	}, nil
+	}
+
+	err = user.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 func (u *User) ValidatePassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
 
 	return err == nil
+}
+
+func (u *User) Validate() error {
+	if u.Name == "" {
+		return ErrNameIsRequired
+	}
+
+	if u.Email == "" {
+		return ErrEmailIsRequired
+	}
+
+	if u.Password == "" {
+		return ErrPasswordIsRequired
+	}
+
+	return nil
 }
