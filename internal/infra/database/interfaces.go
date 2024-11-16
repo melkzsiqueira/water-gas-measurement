181fcf72fd985498f686456001abc9d2bf22de9e
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-11-16 00:22:31 -0300

    feat: add measurement business rule and interface

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(id string) (*entity.User, error)
 }
+
+type MeasurementInterface interface {
+	Create(measurement *entity.Measurement) error
+	FindAll(page, limit int, sort string) ([]entity.Measurement, error)
+	FindById(id string) (*entity.Measurement, error)
+	Update(measurement *entity.Measurement) error
+	Delete(id string) error
+}
---
+++ internal/infra/database/measurement_db.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"github.com/melkzsiqueira/water-gas-measurement/internal/entity"
+	"gorm.io/gorm"
+)
+
+type Measurement struct {
+	DB *gorm.DB
+}
+
+func NewMeasurement(db *gorm.DB) *Measurement {
+	return &Measurement{
+		DB: db,
+	}
+}
+
+func (m *Measurement) Create(measurement *entity.Measurement) error {
+	return m.DB.Create(measurement).Error
+}
+
+func (m *Measurement) FindAll(page, limit int, sort string) ([]entity.Measurement, error) {
+	var measurements []entity.Measurement
+	if sort != "" && sort != "asc" && sort != "desc" {
+		sort = "asc"
+	}
+	if page != 0 {
+		page = 1
+	}
+	if limit != 0 {
+		limit = 10
+	}
+	err := m.DB.Order("created_at " + sort).Offset((page - 1) * limit).Limit(limit).Find(&measurements).Error
+	return measurements, err
+}
+
+func (m *Measurement) FindById(id string) (*entity.Measurement, error) {
+	var measurement entity.Measurement
+	err := m.DB.First(&measurement, "id = ?", id).Error
+	return &measurement, err
+}
+
+func (m *Measurement) Update(measurement *entity.Measurement) error {
+	_, err := m.FindById(measurement.ID.String())
+	if err != nil {
+		return err
+	}
+	return m.DB.Save(measurement).Error
+}
+
+func (m *Measurement) Delete(id string) error {
+	measurement, err := m.FindById(id)
+	if err != nil {
+		return err
+	}
+	return m.DB.Delete(measurement).Error
+}
