4a35839f182f5494b2b58f23331d7fdc6f60ffd1
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-12-01 15:40:39 -0300

    feat: add UserHandler with methods for user authentication and creation

---
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,80 @@
+package handlers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/jwtauth"
+	"github.com/goccy/go-json"
+	"github.com/melkzsiqueira/water-gas-measurement/internal/dto"
+	"github.com/melkzsiqueira/water-gas-measurement/internal/entity"
+	"github.com/melkzsiqueira/water-gas-measurement/internal/infra/database"
+)
+
+type UserHandler struct {
+	UserDB       database.UserInterface
+	Jwt          *jwtauth.JWTAuth
+	JwtExpiresIn int
+}
+
+func NewUserHandler(userDB database.UserInterface, jwt *jwtauth.JWTAuth, jwtExpiresIn int) *UserHandler {
+	return &UserHandler{
+		UserDB:       userDB,
+		Jwt:          jwt,
+		JwtExpiresIn: jwtExpiresIn,
+	}
+}
+
+func (h *UserHandler) GetToken(w http.ResponseWriter, r *http.Request) {
+	var login dto.GetTokenInput
+	err := json.NewDecoder(r.Body).Decode(&login)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(login.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("email or password invalid"))
+		return
+	}
+	if !u.ValidatePassword(login.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("email or password invalid"))
+		return
+	}
+	_, token, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
+	})
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: token,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(u)
+}
