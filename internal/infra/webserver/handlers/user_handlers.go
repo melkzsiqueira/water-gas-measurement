e3ecea8384600666c3fbddc9e4914611d3f9f40c
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-12-02 05:27:35 -0300

    feat: enhance user token and creation handlers with improved error responses

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -15,12 +15,27 @@ type UserHandler struct {
 	UserDB database.UserInterface
 }
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 func NewUserHandler(userDB database.UserInterface) *UserHandler {
 	return &UserHandler{
 		UserDB: userDB,
 	}
 }
 
+// Get token 	godoc
+// @Summary    	Get a user token
+// @Description	Get a user token
+// @Tags       	users
+// @Accept     	json
+// @Produce    	json
+// @Param      	request				body		dto.GetTokenInput	true	"user credentials"
+// @Success    	200					{object}	dto.GetTokenOutput
+// @Failure    	400					{object}	Error
+// @Failure    	401					{object}  	Error
+// @Router     	/users/token		[post]
 func (h *UserHandler) GetToken(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("token").(*jwtauth.JWTAuth)
 	jwtExpiresIn := r.Context().Value("token_expires_in").(int)
@@ -29,48 +44,65 @@ func (h *UserHandler) GetToken(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&login)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	u, err := h.UserDB.FindByEmail(login.Email)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("email or password invalid"))
+		error := Error{Message: "email or password invalid"}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	if !u.ValidatePassword(login.Password) {
 		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("email or password invalid"))
+		error := Error{Message: "email or password invalid"}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	_, token, _ := jwt.Encode(map[string]interface{}{
 		"sub": u.ID.String(),
 		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
 	})
-	accessToken := struct {
-		AccessToken string `json:"access_token"`
-	}{
-		AccessToken: token,
-	}
+	response := &dto.GetTokenOutput{AccessToken: token}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(accessToken)
+	json.NewEncoder(w).Encode(response)
 }
 
+// Create user 	godoc
+// @Summary     Create user
+// @Description	Create user
+// @Tags		users
+// @Accept      json
+// @Produce     json
+// @Param       request	body      dto.CreateUserInput	true	"user request"
+// @Success     201		{object}  entity.User
+// @Failure     400		{object}  Error
+// @Failure     500		{object}  Error
+// @Router      /users	[post]
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	u, err := entity.NewUser(user.Name, user.Email, user.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	err = h.UserDB.Create(u)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
