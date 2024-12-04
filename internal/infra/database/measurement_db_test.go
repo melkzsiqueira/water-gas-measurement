63b76cf0fe17dd2ac44f283beca6449efb86ec0c
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-12-03 23:08:46 -0300

    fix: simplify TestCreateNewMeasurement by removing slice and adjusting Create method call

--- internal/infra/database/measurement_db_test.go
+++ internal/infra/database/measurement_db_test.go
@@ -17,17 +17,12 @@ func TestCreateNewMeasurement(t *testing.T) {
 		t.Fatalf("could not open database: %v", err)
 	}
 	db.AutoMigrate(&entity.Measurement{})
-	var m []entity.Measurement
-	for range 2 {
-		measurements, err := entity.NewMeasurement(19, image, "1", "878ab991-20b0-41c3-9c78-849744e8312a")
-		assert.NoError(t, err)
-		m = append(m, *measurements)
-	}
-	measurementDb := NewMeasurement(db)
-	err = measurementDb.Create(&m)
+	m, err := entity.NewMeasurement(19, image, "1", "878ab991-20b0-41c3-9c78-849744e8312a")
 	assert.NoError(t, err)
-	assert.NotEmpty(t, m[0].ID)
-	assert.NotEmpty(t, m[1].ID)
+	measurementDb := NewMeasurement(db)
+	err = measurementDb.Create(m)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, m.ID)
 }
 
 func TestFindAllMeasurements(t *testing.T) {
