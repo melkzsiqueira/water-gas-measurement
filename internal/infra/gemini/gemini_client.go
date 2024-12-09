d6f7db57f0e7227f2731db31d8f9757c8f187f05
Melkz Siqueira <melkz.siqueira@gmail.com>
2024-12-08 21:53:52 -0300

    refactor: update GeminiClient to Gemini and improve ProcessImage method

--- internal/infra/gemini/gemini_client.go
+++ internal/infra/gemini/gemini_client.go
@@ -4,41 +4,40 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"strings"
 
 	"github.com/google/generative-ai-go/genai"
 	"github.com/melkzsiqueira/water-gas-measurement/internal/dto"
 	"google.golang.org/api/option"
 )
 
-type GeminiClient struct {
-	APIKey string
-	Model  string
+type Gemini struct {
+	model  string
 	Gemini *genai.Client
 }
 
-func NewGeminiClient(apiKey, model string) *GeminiClient {
-	return &GeminiClient{
-		APIKey: apiKey,
-		Model:  model,
-		Gemini: &genai.Client{},
+func NewGeminiClient(apiKey, model string) (*Gemini, error) {
+	client, err := genai.NewClient(context.Background(), option.WithAPIKey(apiKey))
+	if err != nil {
+		return nil, err
 	}
+	return &Gemini{
+		model:  model,
+		Gemini: client,
+	}, err
 }
 
-func (g *GeminiClient) ProcessImage(ctx context.Context, request dto.ProcessImageRequest) (dto.ProcessImageResponse, error) {
-	client, err := genai.NewClient(ctx, option.WithAPIKey(g.APIKey))
-	if err != nil {
-		return dto.ProcessImageResponse{}, err
-	}
-	defer client.Close()
+func (g *Gemini) ProcessImage(request dto.ProcessImageRequest, ctx context.Context) (dto.ProcessImageResponse, error) {
+	defer g.Gemini.Close()
 
-	model := client.GenerativeModel("gemini-1.5-pro-latest")
+	model := g.Gemini.GenerativeModel(g.model)
 	model.ResponseMIMEType = "application/json"
 	model.ResponseSchema = &genai.Schema{
 		Type:  genai.TypeArray,
 		Items: &genai.Schema{Type: genai.TypeString},
 	}
 
-	image, err := base64.StdEncoding.DecodeString(request.Image)
+	image, err := base64.StdEncoding.DecodeString(request.Data)
 	if err != nil {
 		return dto.ProcessImageResponse{}, err
 	}
@@ -46,7 +45,7 @@ func (g *GeminiClient) ProcessImage(ctx context.Context, request dto.ProcessImag
 	resp, err := model.GenerateContent(
 		ctx,
 		genai.Text("You are a meter reading expert. Extract the entire numeric value of a gas or water meter reading from this image in base64. Explicitly return only the integer numeric value."),
-		genai.ImageData("png", image),
+		genai.ImageData(strings.TrimPrefix(request.Mime, "image/"), image),
 	)
 	if err != nil {
 		return dto.ProcessImageResponse{}, err
